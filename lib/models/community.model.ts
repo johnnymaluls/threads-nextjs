f8ee4d961fd18ea48d242ede7e80d43cfbe54e8a
johnny <jmalulan7@gmail.com>
2023-09-17 12:19:50 +0800

    Add clerk webhook for communities, add community actions, and community model

---
+++ lib/models/community.model.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const communitySchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+//First time will create the Community model -> mongoose.model('Community', communitySchema)
+//Second time will only use the created Community model
+const Community =
+  mongoose.models.Community || mongoose.model("Community", communitySchema);
+
+export default Community;
